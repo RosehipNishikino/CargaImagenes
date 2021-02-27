7e5b11e4976fe5dea570b8dbe4ec77530da5b8f7
Sealtiel <sealtielsw@hotmail.com>
2021-02-27 13:44:49 -0600

    Añadido botón para cambiar entre imagenes

--- app/src/main/java/com/example/cargaimagenes/MainActivity.java
+++ app/src/main/java/com/example/cargaimagenes/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cargaimagenes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
+import android.view.View;
 import android.widget.ImageView;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -11,9 +12,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView img;
+    int imagenACargar = 0;
     DisplayImageOptions options;
     ImageLoader imageLoader;
-    String[] heroes = {"https://simplifiedcoding.net/demos/view-flipper/images/spiderman.jpg","https://simplifiedcoding.net/demos/view-flipper/images/spiderman.jpg",
+    String[] heroes = {"https://simplifiedcoding.net/demos/view-flipper/images/spiderman.jpg","https://simplifiedcoding.net/demos/view-flipper/images/ironman.jpg",
     "https://simplifiedcoding.net/demos/view-flipper/images/thor.jpg","https://simplifiedcoding.net/demos/view-flipper/images/wolverine.jpeg"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,22 @@ public class MainActivity extends AppCompatActivity {
         imageLoader.init(ImageLoaderConfiguration.createDefault(getApplicationContext()));
         img = findViewById(R.id.idImageView);
         String imageUri = heroes[0];
+        options = new DisplayImageOptions.Builder()
+                .showStubImage(R.drawable.ic_launcher_foreground)
+                .showImageForEmptyUri(R.drawable.ic_launcher_background)
+                .cacheInMemory()
+                .cacheOnDisc()
+                .build();
+        imageLoader.displayImage(imageUri,img,options,null);
+    }
+    public void Boton(View v){
+        imagenACargar+=1;
+        if(imagenACargar > 3)
+            imagenACargar = 0;
+        imageLoader = ImageLoader.getInstance();
+        imageLoader.init(ImageLoaderConfiguration.createDefault(getApplicationContext()));
+        img = findViewById(R.id.idImageView);
+        String imageUri = heroes[imagenACargar];
         options = new DisplayImageOptions.Builder()
                 .showStubImage(R.drawable.ic_launcher_foreground)
                 .showImageForEmptyUri(R.drawable.ic_launcher_background)
